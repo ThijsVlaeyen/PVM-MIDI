5cf7782ce6c3503b3ed3888928235418deea1c17
ThijsVlaeyen <thijs.vlaeyen@student.ucll.be>
2020-04-20 10:37:05 +0200

    primitive instruments

--- src/midi/midi/primitives.h
+++ src/midi/midi/primitives.h
@@ -7,6 +7,8 @@
 namespace midi
 {
 	struct __declspec(empty_bases)Channel : show_value<Channel, int>, equality<Channel>, tagged<uint8_t, Channel> { using tagged::tagged; };
+	struct __declspec(empty_bases)Instrument : show_value<Instrument, int>, equality<Instrument>, tagged<uint8_t, Instrument> { using tagged::tagged; };
+
 }
 
 #endif
